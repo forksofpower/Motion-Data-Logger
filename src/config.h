0d3c1bd63aa5a37afed673f7339ac75d571f97ff
Patrick Jones <forksofpower@gmail.com>
2023-05-19 19:40:01 -0500

    create common config header

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SD_CS 15
+#define SD_SCK 14
+#define SD_MOSI 13
+#define SD_MISO 12
+#define LOGS_PER_SECOND 50
+#define LOG_DELIMETER ","
+#define LOG_FILENAME "/log.txt"
+
+#endif /* CONFIG_H */
